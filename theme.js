41199d217928c9353e0d572bcd2d3946dedc3aba
Seoyoung Lee <wow9176@naver.com>
2022-08-08 16:24:14 +0900

    Add theme file

---
+++ theme.js
@@ -0,0 +1,12 @@
+export const colors = {
+  mainBlue: "#0047E4",
+  stroke: "#4C83FF",
+  pageTextGray1: "#727272",
+  smallTextGray2: "#AEAEAE",
+  buttonGray3: "#CFCFCF",
+};
+
+export const fontSizes = {
+  bigButton: 20,
+  smallButton: 18,
+};
