9071b4a31f37413005aa0d0992d905517f8a9d45
Seoyoung Lee <wow9176@naver.com>
2022-09-06 00:53:25 +0900

    #22 feat: 지원하기 메인 화면 구현

--- screens/helper/index.js
+++ screens/helper/index.js
@@ -1,6 +1,6 @@
 import Home from "./Home";
 import QuickApply from "./apply/QuickApply";
-import ReservationApply from "./ReservationApply";
+import ReservationApply from "./apply/ReservationApply";
 import HelperChat from "./HelperChat";
 import HelperApplyList from "./HelperApplyList";
 import MyPage from "./MyPage";
