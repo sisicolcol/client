a99efb71336a5c1117f26e83d473377348c47f78
listenyoon <listenyoon@gmail.com>
2022-09-05 11:51:21 +0900

    #20 build: eas build 알림 구현 초기세팅

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -4,12 +4,147 @@ import BlindStack from "./BlindStack";
 import AuthStack from "./AuthStack";
 import HelperTab from "./HelperTab";
 
+import { Button, Platform, Text, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
 const Navigation = () => {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      // fetch(PUSH_ENDPOINT, {
+      // 	method: 'POST',
+      // 	headers: {
+      // 		Accept: 'application/json',
+      // 		'Content-Type': 'application/json',
+      // 	},
+      // 	body: JSON.stringify({
+      // 		token: {
+      // 			value: token,
+      // 		}
+      // 	}),
+      // })
+      // 	.then(() => console.log('send!'))
+      // 	.catch((err) => console.log(err));
+      setExpoPushToken(token);
+    });
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (
+        typeof notificationListener.current !== "undefined" &&
+        typeof responseListener.current !== "undefined"
+      ) {
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+        Notifications.removeNotificationSubscription(responseListener.current);
+      }
+    };
+  }, []);
+
   return (
-    <NavigationContainer>
-      <BlindStack />
-    </NavigationContainer>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-around",
+      }}
+    >
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Text>
+          Title: {notification && notification.request.content.title}{" "}
+        </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>
+          Data:{" "}
+          {notification && JSON.stringify(notification.request.content.data)}
+        </Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
+    </View>
   );
 };
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      sound: "default",
+      title: "You've got mail! 📬",
+      body: "Here is the notification body",
+      data: { data: "goes here" },
+    },
+    trigger: null,
+    // trigger:{seconds:5}
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
+
+// const Navigation = () => {
+//   return (
+//     // <NavigationContainer>
+//     //   <BlindStack />
+//     // </NavigationContainer>
+//   );
+// };
 
 export default Navigation;
